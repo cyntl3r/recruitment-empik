f4c42c68e3165d7230182d726848bd67e60542c9
cyntl3r <damian@cyntler.com>
2020-11-21 09:42:06 +0100

    Update App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { ProductContext } from '../../contexts/productContext';
+import { useApiRequest } from '../../hooks/useApiRequest';
+import { getCartService } from '../../services';
+import { calculateTotalOrder } from '../../utils/calculateTotalOrder';
+import { priceFormat } from '../../utils/priceFormat';
+import { Loader } from '../Loader/Loader';
+import { ProductList } from '../ProductList/ProductList';
 import './App.css';
 
-const App = () => {
+export const App = () => {
+  const { setFetchedProducts } = useContext(ProductContext);
+  const {
+    makeRequest: makeGetCartRequest,
+    data: products,
+    loading,
+  } = useApiRequest({
+    service: getCartService,
+    initialData: [],
+    initialLoading: true,
+  });
+
+  useEffect(() => {
+    makeGetCartRequest();
+  }, []);
+
+  useEffect(() => {
+    if (products) {
+      setFetchedProducts(products);
+    }
+  }, [products]);
+
   return (
-    <div className="container">
+    <div className="app">
       <h3>Lista produktów</h3>
-      <ul>
-        <li className="row">Patelnia, cena: 89,99zł</li>
-      </ul>
+      {loading ? <Loader /> : <ProductList data={products} />}
+      <h4>Suma zamówienia: {priceFormat(calculateTotalOrder(products))}</h4>
     </div>
   );
 };
-
-export { App };
