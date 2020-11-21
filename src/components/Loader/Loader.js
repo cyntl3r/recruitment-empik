b38d04b049524d74bd29aa77887029f133ad8354
cyntl3r <damian@cyntler.com>
2020-11-21 09:41:13 +0100

    Add CSS loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+export const Loader = () => (
+  <div className="loader">
+    <div className="loader__lds-facebook">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
